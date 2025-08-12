dca0cc936cff394a6930a92a3efe31d4938bcc92
IdenTiclla <iden.ticlla@gmail.com>
2025-08-12 18:37:20 -0400

    Refactor homepage tests to utilize HomePage page object model for improved readability and maintainability; streamline navigation and expectation assertions.

--- tests/test-homepage.spec.js
+++ tests/test-homepage.spec.js
@@ -1,13 +1,21 @@
 import { test, expect } from "@playwright/test"
+import HomePage from "../pages/home-page"
 
 test.describe("Homepage", () => {
-    test("test homepage title", async ({ page }) => {
-        await page.goto("/")
-        expect(page).toHaveTitle("Your Store")
+    let homePage;
+
+    test.beforeEach(async ({ page }) => {
+        homePage = new HomePage(page)
     })
+
+    test("test homepage title", async ({ page }) => {
+        await homePage.navigate()
+        await expect(page).toHaveTitle("Your Store")
+    })
+    
     test("Test homepage url", async ({ page }) => {
-        await page.goto("/")
-        expect(page.url()).toContain("https://ecommerce-playground.lambdatest.io/")
+        await homePage.navigate()
+        await expect(page.url()).toContain("https://ecommerce-playground.lambdatest.io/")
     })
 })
 
