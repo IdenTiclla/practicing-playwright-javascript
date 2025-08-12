0338304b2f2096fbaa4d6ba9697a914bd8df4621
IdenTiclla <iden.ticlla@gmail.com>
2025-08-12 18:20:23 -0400

    Enhance login tests by integrating @faker-js/faker for dynamic credential generation; remove empty credentials test case for improved clarity and focus on valid/invalid scenarios.

--- tests/test-login.spec.js
+++ tests/test-login.spec.js
@@ -2,6 +2,7 @@ import { test, expect } from "@playwright/test"
 import HomePage from "../pages/home-page"
 import LoginPage from "../pages/login-page"
 import dotenv from 'dotenv'
+import { faker } from "@faker-js/faker";
 
 dotenv.config()
 
@@ -20,20 +21,15 @@ test.describe("Login",() => {
         await loginPage.login(process.env.VALID_EMAIL, process.env.VALID_PASSWORD)
         await expect(page.url()).toContain("https://ecommerce-playground.lambdatest.io/index.php?route=account/account")    
     })
+
     test("test login with invalid credentials", async ({ page }) => {
         await homePage.navigate();
         await homePage.navbar.clickLogin();
-        await loginPage.login("invalidemail16@gmail.com", "someinvalidpassword");
+        const randomEmail = faker.internet.email();
+        const randomPassword = faker.internet.password();
+        await loginPage.login(randomEmail, randomPassword);
         expect(await loginPage.alertError.isVisible()).toBe(true);
         expect(await loginPage.alertError.getErrorMessage()).toContain("Warning: No match for E-Mail Address and/or Password.")
         await expect(page.url()).not.toContain("https://ecommerce-playground.lambdatest.io/index.php?route=account/account")    
     })
-
-    test("test login with empty credentials", async ({ page }) => {
-        await homePage.navigate()
-        await homePage.navbar.clickLogin()
-        await loginPage.login("", "")
-        await expect(page.getByText("Warning: No match for E-Mail Address and/or Password.")).toBeVisible()
-        await expect(page.url()).toContain("https://ecommerce-playground.lambdatest.io/index.php?route=account/login")    
-    })      
 })
